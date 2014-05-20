dda55f20088079765312e7b920411220bb9d8b44
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-05-20 09:32:04 -0400

    passing tests with refactored code

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ var ss = require('simple-statistics')
 var inside = require('turf-inside')
 
 module.exports = function(polyFC, ptFC, inField, outField, done){
-  done = done || function () {};
-
   polyFC.features.forEach(function(poly){
     if(!poly.properties){
       poly.properties = {}
     }
     var values = []
     ptFC.features.forEach(function(pt){
-      if (t.inside(pt, poly)) {
+      if (inside(pt, poly)) {
         values.push(pt.properties[inField]);
       }
     })
     poly.properties[outField] = ss.average(values)
   })
 
-  done(null, polyFC)
   return polyFC;
 }
