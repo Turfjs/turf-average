e5fcfb06b223d5d38020295713e2932fcd7b1243
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-02 17:11:11 -0800

    Switch example to geojson primitives

--- index.js
+++ index.js
@@ -10,35 +10,99 @@ var inside = require('turf-inside');
  * @param {string} outputField the field in the `polygons` FeatureCollection to put results of the averages
  * @return {FeatureCollection} a FeatureCollection of {@link Polygon} features with the value of `outField` set to the calculated average
  * @example
- * var polygons = turf.featurecollection([
- *  turf.polygon([[
- *    [10.666351, 59.890659],
- *    [10.666351, 59.936784],
- *    [10.762481, 59.936784],
- *    [10.762481, 59.890659],
- *    [10.666351, 59.890659]
- *  ]]),
- *  turf.polygon([[
- *    [10.764541, 59.889281],
- *    [10.764541, 59.937128],
- *    [10.866165, 59.937128],
- *    [10.866165, 59.889281],
- *    [10.764541, 59.889281]
- *  ]])
- * ]);
- * var points = turf.featurecollection([
- *  turf.point([10.724029, 59.926807], {population: 200}),
- *  turf.point([10.715789, 59.904778], {population: 600}),
- *  turf.point([10.746002, 59.908566], {population: 100}),
- *  turf.point([10.806427, 59.908910], {population: 200}),
- *  turf.point([10.79544, 59.931624], {population: 300})
- * ]);
+* var polygons = {
+ *   "type": "FeatureCollection",
+ *   "features": [
+ *     {
+ *       "type": "Feature",
+ *       "properties": {},
+ *       "geometry": {
+ *         "type": "Polygon",
+ *         "coordinates": [[
+ *           [10.666351, 59.890659],
+ *           [10.666351, 59.936784],
+ *           [10.762481, 59.936784],
+ *           [10.762481, 59.890659],
+ *           [10.666351, 59.890659]
+ *         ]]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {},
+ *       "geometry": {
+ *         "type": "Polygon",
+ *         "coordinates": [[
+ *           [10.764541, 59.889281],
+ *           [10.764541, 59.937128],
+ *           [10.866165, 59.937128],
+ *           [10.866165, 59.889281],
+ *           [10.764541, 59.889281]
+ *         ]]
+ *       }
+ *     }
+ *   ]
+ * };
+ * var points = {
+ *   "type": "FeatureCollection",
+ *   "features": [
+ *     {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "population": 200
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [10.724029, 59.926807]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "population": 600
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [10.715789, 59.904778]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "population": 100
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [10.746002, 59.908566]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "population": 200
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [10.806427, 59.908910]
+ *       }
+ *     }, {
+ *       "type": "Feature",
+ *       "properties": {
+ *         "population": 300
+ *       },
+ *       "geometry": {
+ *         "type": "Point",
+ *         "coordinates": [10.79544, 59.931624]
+ *       }
+ *     }
+ *   ]
+ * };
  *
  * var averaged = turf.average(
  *  polygons, points, 'population', 'pop_avg');
  *
- * var result = turf.featurecollection(
- *  points.features.concat(averaged.features));
+ * var resultFeatures = points.features.concat(
+ *   averaged.features);
+ * var result = {
+ *   "type": "FeatureCollection",
+ *   "features": resultFeatures
+ * };
  *
  * //=result
  */
